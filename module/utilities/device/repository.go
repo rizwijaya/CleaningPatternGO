6646fe733c366de8f9a34c1be22636c85bdcb340
Rizqi Wijaya <rizwijaya@github.com>
2022-04-27 12:35:30 +0700

    Create Repository Device Find By ID.

--- module/utilities/device/repository.go
+++ module/utilities/device/repository.go
@@ -4,6 +4,7 @@ import "gorm.io/gorm"
 
 type Repository interface {
 	GetAllDevice() ([]Device, error)
+	FindByID(ID int) (Device, error)
 }
 
 type repository struct {
@@ -18,6 +19,17 @@ func (r *repository) GetAllDevice() ([]Device, error) {
 	var device []Device
 
 	err := r.db.Find(&device).Error
+	if err != nil {
+		return device, err
+	}
+
+	return device, nil
+}
+
+func (r *repository) FindByID(ID int) (Device, error) {
+	var device Device
+
+	err := r.db.Where("no_device = ?", ID).Find(&device).Error
 	if err != nil {
 		return device, err
 	}
