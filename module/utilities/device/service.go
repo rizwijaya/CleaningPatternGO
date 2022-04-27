3b4e8e9cf9797efddf59e5f20be11cc6472aac00
Rizqi Wijaya <rizwijaya@github.com>
2022-04-27 12:35:09 +0700

    Create Service Get Device By ID

--- module/utilities/device/service.go
+++ module/utilities/device/service.go
@@ -1,7 +1,10 @@
 package device
 
+import "errors"
+
 type Service interface {
 	GetAllDevice() ([]Device, error)
+	GetDeviceByID(ID int) (Device, error)
 }
 
 type service struct {
@@ -16,6 +19,19 @@ func (s *service) GetAllDevice() ([]Device, error) {
 	device, err := s.repository.GetAllDevice()
 	if err != nil {
 		return device, err
+	}
+
+	return device, nil
+}
+
+func (s *service) GetDeviceByID(ID int) (Device, error) {
+	device, err := s.repository.FindByID(ID)
+	if err != nil {
+		return device, err
+	}
+
+	if device.No_device == 0 {
+		return device, errors.New("No Device found on with that ID")
 	}
 
 	return device, nil
